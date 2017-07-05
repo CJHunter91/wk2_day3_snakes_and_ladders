3979d696a6142cc4ddd6e918de32d2bac0f99ace
Chris Hunter <orkem2@gmail.com>
2017-07-05 12:02:26 +0100

    Added ladder class and test top and bottom passed

---
+++ ladder.rb
@@ -0,0 +1,12 @@
+class Ladder
+
+attr_reader :top_pos, :bottom_pos
+
+  def initialize(bottom_pos, top_pos)
+    @top_pos = top_pos
+    @bottom_pos = bottom_pos
+  end
+
+  
+
+end
--- spec/ladder_spec.rb
+++ spec/ladder_spec.rb
@@ -6,7 +6,15 @@ require_relative("../ladder.rb")
 require_relative("../board.rb")
 
 class TestLadder < MiniTest::Test
-  def setup
-    
+    def setup
+      @Ladder1 = Ladder.new(5, 8)
+    end
+
+    def test_Ladder_top_position
+      assert_equal(8, @Ladder1.top_pos)
+    end
+
+    def test_Ladder_bottom_position
+      assert_equal(5, @Ladder1.bottom_pos)
+    end
   end
-end
