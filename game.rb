33c70955e559b30863bea6ca9186716df9e86a1c
Chris Hunter <orkem2@gmail.com>
2017-07-05 19:37:21 +0100

    Added string function that prints out player move

--- game.rb
+++ game.rb
@@ -36,13 +36,18 @@ class Game
       if (@player.position_on_board == ladder.bottom_pos)
         @player.position_on_board = ladder.top_pos
         print "Yay you go up the ladder."
-        print @player.name + "moved to " + @player.position_on_board.to_s
+        player_pos_str
       end
     end
   end
 
   def player_roll_and_move
     @player.move(@board_obj.die.roll)
+    player_pos_str
+  end
+
+  def player_pos_str()
+    print @player.name + "moved to " + @player.position_on_board.to_s
   end
 
   def have_won?()
